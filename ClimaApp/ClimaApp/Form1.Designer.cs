79ab03d043dc0bb21d6e5ee020d25709c1455759
pegadinhas <pedrocv149@gmail.com>
2025-09-01 22:42:43 -0300

    ClimaApp projeto inicial

---
+++ ClimaApp/ClimaApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ClimaApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelTitulo = new Label();
+            textBox1 = new TextBox();
+            labelTexto = new Label();
+            labelCidade = new Label();
+            SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            labelTitulo.AutoSize = true;
+            labelTitulo.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelTitulo.Location = new Point(12, 9);
+            labelTitulo.Name = "labelTitulo";
+            labelTitulo.Size = new Size(125, 32);
+            labelTitulo.TabIndex = 0;
+            labelTitulo.Text = "ClimaAPP";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.Location = new Point(79, 150);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(159, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // labelTexto
+            // 
+            labelTexto.AutoSize = true;
+            labelTexto.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelTexto.Location = new Point(12, 79);
+            labelTexto.Name = "labelTexto";
+            labelTexto.Size = new Size(439, 42);
+            labelTexto.TabIndex = 2;
+            labelTexto.Text = "Verifique o clima da cidade que você desejar, basta escrever o \r\nlugar que você deseja verificar";
+            // 
+            // labelCidade
+            // 
+            labelCidade.AutoSize = true;
+            labelCidade.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelCidade.Location = new Point(12, 150);
+            labelCidade.Name = "labelCidade";
+            labelCidade.Size = new Size(61, 21);
+            labelCidade.TabIndex = 3;
+            labelCidade.Text = "Cidade:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ScrollBar;
+            ClientSize = new Size(534, 661);
+            Controls.Add(labelCidade);
+            Controls.Add(labelTexto);
+            Controls.Add(textBox1);
+            Controls.Add(labelTitulo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelTitulo;
+        private TextBox textBox1;
+        private Label labelTexto;
+        private Label labelCidade;
+    }
+}
