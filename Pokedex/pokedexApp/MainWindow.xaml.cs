44f937ddc35c42e33f0182b450476d7addec5bf4
pegadinhas <pedrocv149@gmail.com>
2025-09-08 23:37:24 -0300

    Correções no código

--- Pokedex/pokedexApp/MainWindow.xaml.cs
+++ Pokedex/pokedexApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text;
 using System.Windows;
+using Newtonsoft.Json;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -7,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Linq;
 using System.Windows.Shapes;
 
 namespace pokedexApp
@@ -16,19 +19,34 @@ namespace pokedexApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class PokemonResult
+        {
+            public string name { get; set; }
+
+            public int id { get; set; }
+
+            public string type { get; set; }
+
+            public string url { get; set; }
+        }
+
+        public class PokemonApiResponse
+        {
+            public int count { get; set; }
+            public List<PokemonResult> results { get; set; }
+        }
+
+        List<string> PokemonNomes = new List<string>();
+        CollectionView view;
+
         public MainWindow()
         {
             InitializeComponent();
+            CarregarPokemons();
+        }
+        private void CarregarPokemons()
+        {
 
-            List<string> pokemons = new List<string>
-            {
-                "Bulbasaur", "Ivysaur", "Venusaur",
-                "Charmander", "Charmeleon", "Charizard",
-                "Squirtle", "Wartortle", "Blastoise",
-                "Pikachu", "Raichu"
-            };
-
-            PokemonComboBox.ItemsSource = pokemons;
         }
     }
 }
