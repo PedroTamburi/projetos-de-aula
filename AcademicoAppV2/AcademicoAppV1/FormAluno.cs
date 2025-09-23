8845007fc601fa182bdb50370941ef6638307a71
Pedro Tamburi <55924306+PedroTamburi@users.noreply.github.com>
2025-09-23 11:26:01 -0300

    Término do CRUD

---
+++ AcademicoAppV2/AcademicoAppV1/FormAluno.cs
@@ -0,0 +1,232 @@
+﻿using ReaLTaiizor.Controls;
+using ReaLTaiizor.Forms;
+using ReaLTaiizor.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcademicoAppV1
+{
+    public partial class FormAluno : MaterialForm
+    {
+        #region Variaveis
+        string alunoFileName = "alunos.txt";
+        bool isEditMode = false;
+        int indexSelecionado = 0;
+        #endregion
+
+        #region Métodos
+        public FormAluno()
+        {
+            InitializeComponent();
+        }
+
+        private void limparCampo()
+        {
+            isEditMode = false;
+            foreach (var control in tabPageCadastro.Controls)
+            {
+                if (control is MaterialTextBoxEdit textBox)
+                    textBox.Clear();
+                if (control is MaterialMaskedTextBox maskedTextBox)
+                    maskedTextBox.Clear();
+            }
+        }
+
+        private void Salvar()
+        {
+            var line = $"{txtMatricula.Text};" +
+                $"{mtxtDataNascimento.Text};" +
+                $"{txtNome.Text};" +
+                $"{txtEndereco.Text};" +
+                $"{txtBairro.Text};" +
+                $"{txtCidade.Text};" +
+                $"{txtSenha.Text};" +
+                $"{cbEstado.Text};";
+
+            if (!isEditMode)
+            {
+                using (StreamWriter sw = new StreamWriter(alunoFileName, true))
+                {
+                    sw.WriteLine(line);
+                }
+            }
+            else
+            {
+                var fileLines = File.ReadAllLines(alunoFileName).ToList();
+                fileLines[indexSelecionado] = line;
+                File.WriteAllLines(alunoFileName, fileLines);
+            }
+        }
+
+        private bool ValidaFormulario()
+        {
+            var erro = "";
+
+
+            if (string.IsNullOrEmpty(txtMatricula.Text))
+            {
+                erro += "Matricula deve ser informada!\n";
+            }
+            if (string.IsNullOrEmpty(txtNome.Text))
+            {
+                erro += "Nome deve ser informada!\n";
+            }
+            if (string.IsNullOrEmpty(mtxtDataNascimento.Text) || mtxtDataNascimento.Text.Contains("_"))
+            {
+                erro += "Data de nascimento deve ser informada!\n";
+            }
+            if (string.IsNullOrEmpty(txtEndereco.Text))
+            {
+                erro += "Endereço deve ser informada!\n";
+            }
+            if (string.IsNullOrEmpty(txtBairro.Text))
+            {
+                erro += "Bairro deve ser informada!\n";
+            }
+            if (string.IsNullOrEmpty(txtCidade.Text))
+            {
+                erro += "Cidade deve ser informada!\n";
+            }
+            if (string.IsNullOrEmpty(txtSenha.Text))
+            {
+                erro += "Senha deve ser informada!\n";
+            }
+            if (!string.IsNullOrEmpty(erro))
+            {
+                MessageBox.Show(erro, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void CarregaListView()
+        {
+            listViewConsulta.View = View.Details;
+
+            Cursor.Current = Cursors.WaitCursor;
+            listViewConsulta.Items.Clear();
+            listViewConsulta.Columns.Clear();
+            listViewConsulta.Columns.Add("Matrícula");
+            listViewConsulta.Columns.Add("Data Nasc.");
+            listViewConsulta.Columns.Add("Nome");
+            listViewConsulta.Columns.Add("Endereço");
+            listViewConsulta.Columns.Add("Bairro");
+            listViewConsulta.Columns.Add("Cidade");
+            listViewConsulta.Columns.Add("Senha");
+            listViewConsulta.Columns.Add("Estado");
+            var fileLines = File.ReadAllLines(alunoFileName);
+
+            foreach (var line in fileLines)
+            {
+                var campos = line.Split(";");
+                listViewConsulta.Items.Add(new ListViewItem(campos));
+            }
+            listViewConsulta.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void Editar()
+        {
+
+            if (listViewConsulta.SelectedIndices.Count > 0)
+            {
+                isEditMode = true;
+                indexSelecionado = listViewConsulta.SelectedItems[0].Index;
+                var item = listViewConsulta.SelectedItems[0];
+                txtMatricula.Text = item.SubItems[0].Text;
+                mtxtDataNascimento.Text = item.SubItems[1].Text;
+                txtNome.Text = item.SubItems[2].Text;
+                txtEndereco.Text = item.SubItems[3].Text;
+                txtBairro.Text = item.SubItems[4].Text;
+                txtCidade.Text = item.SubItems[5].Text;
+                txtSenha.Text = item.SubItems[6].Text;
+                cbEstado.Text = item.SubItems[7].Text;
+                tabControlCadastro.SelectedIndex = 0;
+                txtMatricula.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Selecione algum registro!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Deletar()
+        {
+            if(listViewConsulta.SelectedItems.Count > 0)
+            {
+                if(MessageBox.Show("Deseja realmente deletar?", "Pergunta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    var fileLines = File.ReadAllLines(alunoFileName).ToList();
+                    fileLines.RemoveAt(indexSelecionado);
+                    File.WriteAllLines(alunoFileName, fileLines);
+                }
+            }
+        }
+        #endregion
+
+        #region Eventos
+
+        private void materialButton1_Click(object sender, EventArgs e)
+        {
+            limparCampo();
+
+            //mudando par apag cadastro
+            tabControlCadastro.SelectedIndex = 0;
+            //campo matricula recebe foco de teclado
+            txtMatricula.Focus();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Informações não salvas serão perdidas!\n" +
+                "Deseja realmente cancelar?", "Atenção",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                limparCampo();
+                tabControlCadastro.SelectedIndex = 1;
+                isEditMode = false;
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (ValidaFormulario())
+            {
+                Salvar();
+                tabControlCadastro.SelectedIndex = 1;
+            }
+        }
+
+        private void tabPageConsulta_Enter(object sender, EventArgs e)
+        {
+            CarregaListView();
+        }
+
+        private void materialButton2_Click(object sender, EventArgs e)
+        {
+            Editar();
+        }
+
+        private void listViewConsulta_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Editar();
+        }
+
+        private void materialButton3_Click(object sender, EventArgs e)
+        {
+            Deletar();
+            CarregaListView();
+        }
+        #endregion
+    }
+}
---
+++ AcademicoAppV2/AcademicoAppV1/FormPrincipal.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace AcademicoAppV1
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            menuStrip1 = new MenuStrip();
+            opçõesToolStripMenuItem = new ToolStripMenuItem();
+            alunosToolStripMenuItem = new ToolStripMenuItem();
+            professoresToolStripMenuItem = new ToolStripMenuItem();
+            cursosToolStripMenuItem = new ToolStripMenuItem();
+            relatóriosToolStripMenuItem = new ToolStripMenuItem();
+            alunosToolStripMenuItem1 = new ToolStripMenuItem();
+            ajudaToolStripMenuItem = new ToolStripMenuItem();
+            sobreToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { opçõesToolStripMenuItem, relatóriosToolStripMenuItem, ajudaToolStripMenuItem });
+            menuStrip1.Location = new Point(3, 64);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(794, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            opçõesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { alunosToolStripMenuItem, professoresToolStripMenuItem, cursosToolStripMenuItem });
+            opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            opçõesToolStripMenuItem.Size = new Size(59, 20);
+            opçõesToolStripMenuItem.Text = "Opções";
+            // 
+            // alunosToolStripMenuItem
+            // 
+            alunosToolStripMenuItem.Name = "alunosToolStripMenuItem";
+            alunosToolStripMenuItem.Size = new Size(180, 22);
+            alunosToolStripMenuItem.Text = "Alunos";
+            alunosToolStripMenuItem.Click += alunosToolStripMenuItem_Click;
+            // 
+            // professoresToolStripMenuItem
+            // 
+            professoresToolStripMenuItem.Name = "professoresToolStripMenuItem";
+            professoresToolStripMenuItem.Size = new Size(180, 22);
+            professoresToolStripMenuItem.Text = "Professores";
+            // 
+            // cursosToolStripMenuItem
+            // 
+            cursosToolStripMenuItem.Name = "cursosToolStripMenuItem";
+            cursosToolStripMenuItem.Size = new Size(180, 22);
+            cursosToolStripMenuItem.Text = "Cursos";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            relatóriosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { alunosToolStripMenuItem1 });
+            relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            relatóriosToolStripMenuItem.Size = new Size(71, 20);
+            relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // alunosToolStripMenuItem1
+            // 
+            alunosToolStripMenuItem1.Name = "alunosToolStripMenuItem1";
+            alunosToolStripMenuItem1.Size = new Size(111, 22);
+            alunosToolStripMenuItem1.Text = "Alunos";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            ajudaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { sobreToolStripMenuItem });
+            ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            ajudaToolStripMenuItem.Size = new Size(50, 20);
+            ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            sobreToolStripMenuItem.Size = new Size(104, 22);
+            sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.logo;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "FormPrincipal";
+            Text = "Form do pegadinhas e do miguel";
+            WindowState = FormWindowState.Maximized;
+            FormClosing += FormPrincipal_FormClosing;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem opçõesToolStripMenuItem;
+        private ToolStripMenuItem alunosToolStripMenuItem;
+        private ToolStripMenuItem professoresToolStripMenuItem;
+        private ToolStripMenuItem cursosToolStripMenuItem;
+        private ToolStripMenuItem relatóriosToolStripMenuItem;
+        private ToolStripMenuItem alunosToolStripMenuItem1;
+        private ToolStripMenuItem ajudaToolStripMenuItem;
+        private ToolStripMenuItem sobreToolStripMenuItem;
+    }
+}
